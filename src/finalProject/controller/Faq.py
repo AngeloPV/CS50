39025fd2ae317c64aa2301064df8f033b5de2659
AngeloPV <155500288+AngeloPV@users.noreply.github.com>
2024-10-02 16:35:35 -0300

    Delete src directory

--- src/finalProject/controller/Faq.py
+++
@@ -1,8 +0,0 @@
-from ..renderer import template_render
-
-class Faq:
-    @staticmethod
-    def index():
-        return template_render('faq.html')
-
-
--- src/finalProject/controller/Teste.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request
-from ..renderer import template_render
-
-class Teste:
-    @staticmethod
-    def teste(parameter):
-        name = request.args.get("name", parameter)
-        return render_template("index.html", name=parameter)
